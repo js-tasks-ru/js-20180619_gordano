8574f681adce8cb6138ce789009e37b15d7d5b81
Vitaly Goryachev <gordano@jetrockets.ru>
2018-06-25 22:51:14 +0300

    Module 1, Task 1 - done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -6,4 +6,11 @@
  */
 function pow (m, n) {
 
+  let params = [m,n] = [parseInt(m), parseInt(n)]
+
+  if ((isNaN(m)) || isNaN(n) ){
+    return undefined
+  }else{
+    return (m === 0 || n === 0 ) ? 0 : (n != 1) ? m*pow(m, Math.abs(n) -1): m ;
+  }
 }
