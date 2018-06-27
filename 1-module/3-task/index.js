3cf265013ca768df93100f0edf6db7f7e8a172d7
Vitaly Goryachev <gordano@jetrockets.ru>
2018-06-27 16:13:53 +0300

    module 1 task 3 v1

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,40 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+function getMinMax(string) {
+  let floats = getFloatsFromString(string);
 
+  let max = Math.max.apply(Math, floats);
+  let min = Math.min.apply(Math, floats);
+
+  return {min: min, max: max};
 }
+
+function getFloatsFromString(string) {
+  let floatsArray = [];
+  let tempFloat = '';
+  let lettersArray = (string + ' ').split('');
+
+  lettersArray.forEach(function(letter){
+    if (isLetterPartOfFloat(letter, tempFloat)){
+      tempFloat += letter;
+    }
+    else{
+      if (tempFloat != ''){
+        floatsArray.push(parseFloat(tempFloat));
+        tempFloat = '';
+      };
+    };
+  });
+
+  return floatsArray;
+};
+
+function isLetterPartOfFloat(letter, tempFloat){
+  if (letter == '-' && tempFloat.length == 0){ return true };
+  if (letter == '.' && !tempFloat.includes('.')){ return true };
+  return isNumber(letter);
+}
+
+let isNumber = (letter) => !isNaN(parseInt(letter));
 
