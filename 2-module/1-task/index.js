9cb7b7c3a3a975f5737c20ad5d1cd5356e2276f6
Vitaly Goryachev <gordano@jetrockets.ru>
2018-06-28 20:45:17 +0300

    module 2 task1 v1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,16 @@
  * @returns {Object}
  */
 function clone (obj) {
+
+  let new_object = JSON.parse(stringifyObject(obj));
+
+  return new_object;
+
+}
+
+function stringifyObject(obj){
+
+  let tmp_object = JSON.stringify(obj);
+
+  return tmp_object;
 }
